94100a947e2e53e3e7860b925092677a1adfc680
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-04-02 15:25:13 -0400

    FEAT: Add Clear Button
    Add clear button to reset grid
    
    REFACTOR: Make an add function to call

--- sricpts.js
+++ sricpts.js
@@ -1,22 +1,44 @@
 const grid = document.querySelector("#grid");
-let size = grid.value;
+const classGrid = document.querySelector(".grid-container");
+const clearButton = document.querySelector("#clear");
 
+let size = grid.value;
 document.querySelector(".grid-size").innerHTML= size + " x " + size;
+gridAdd();
+function gridClear(){
+    let gridSquares = grid.value;
+    while (classGrid.firstChild){
+        classGrid.removeChild(classGrid.lastChild);
+    }
+
+}
+
+function gridAdd(){
+
+    let gridSquares = grid.value;
+    let pixels = (600/gridSquares);
+    for (let i = 0 ; i < gridSquares*gridSquares ; i++){
+        let gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-square");
+        gridSquare.innerHTML="";
+        classGrid.appendChild(gridSquare);
+        gridSquare.style.width = pixels +"px";
+        gridSquare.style.height = pixels +"px";
+    }
+
+}
 
 grid.addEventListener("mousemove",(e) =>{
-    let gridSquares = grid.value;
+    let gridSquares = grid.value;  
+    gridClear();
+    gridAdd()
     document.querySelector(".grid-size").innerHTML= gridSquares + " x " + gridSquares;
-
-   
-
 })
 
-
-
-
-
-
-    
+clearButton.addEventListener("click",(e) =>{
+    gridClear();
+    gridAdd();
+})
 
 
 
