4ef2b6da4511527f396956ec9ac90f17a1a702bc
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-04-04 12:37:44 -0400

    FEAT: Change Color
    FEAT: RGB changer

--- sricpts.js
+++ sricpts.js
@@ -1,20 +1,30 @@
 const grid = document.querySelector("#grid");
 const classGrid = document.querySelector(".grid-container");
 const clearButton = document.querySelector("#clear");
+const gridColor = document.querySelector("#colorPicker");
+const rainbowBtn = document.querySelector("#rainbow");
+const containerOptions = document.querySelector(".container-options");
+const containerMain = document.querySelector(".container-main");
+
+
+let gridChangeColor;
 
 let size = grid.value;
+let colorPicker = gridColor.value;
+
 document.querySelector(".grid-size").innerHTML= size + " x " + size;
+
 gridAdd();
+colorChange();
+
 function gridClear(){
-    let gridSquares = grid.value;
-    while (classGrid.firstChild){
+    
+        while (classGrid.firstChild){
         classGrid.removeChild(classGrid.lastChild);
     }
-
 }
-
 function gridAdd(){
-
+    colorPicker = gridColor.value;
     let gridSquares = grid.value;
     let pixels = (600/gridSquares);
     for (let i = 0 ; i < gridSquares*gridSquares ; i++){
@@ -24,11 +34,15 @@ function gridAdd(){
         classGrid.appendChild(gridSquare);
         gridSquare.style.width = pixels +"px";
         gridSquare.style.height = pixels +"px";
-    }
-
+        
+        document.getElementsByClassName('grid-square')[i].addEventListener("mouseover", function (event) {
+            
+            event.target.style.backgroundColor = colorPicker;       
+    })
+    
+ }
 }
-
-grid.addEventListener("mousemove",(e) =>{
+grid.addEventListener("click",(e) =>{
     let gridSquares = grid.value;  
     gridClear();
     gridAdd()
@@ -38,7 +52,38 @@ grid.addEventListener("mousemove",(e) =>{
 clearButton.addEventListener("click",(e) =>{
     gridClear();
     gridAdd();
+    colorChange();
+    
 })
 
+containerOptions.addEventListener("mouseleave",(e) =>{
+      if(rainbowBtn.checked){
+        colorPicker = colorRGB();
+      }
+      else{
+        colorPicker = gridColor.value;
+      }
+      
+})
 
+function colorChange(){
+    gridChangeColor = document.querySelectorAll(".grid-square");
+}
 
+function colorRGB(){
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    var color = "rgb(" + red + "," + green + "," + blue + ")"; 
+    return color;
+   
+}
+
+containerMain.addEventListener("mousemove",(e)=>{
+    if(rainbowBtn.checked){
+        colorPicker = colorRGB();
+      }
+      else{
+        colorPicker = gridColor.value;
+      }
+})
